bf2df232899d486332e37cb71075d19856e95cc0
Arnav Singh Negi <arnav.n@waferwire.com>
2026-05-15 12:32:57 +0530

    Add comment in app.py to trigger pipeline

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 from flask import Flask
 
+# 15 May 2026 self hosted agent practice: this comment 
+# acts as a minor update such that
+# when this new app.py is committed to GitHub
+# it will trigger a pipeline run which build
+# this simple flask app and deploy it to a VM (pipeline agent)
+# which hosts this app 
+
 app = Flask(__name__)
 
 @app.route("/")
